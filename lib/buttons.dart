62f99cc1cf4cb51870ac21310b662f0a87babb88
VimarshanaBandara <vimarshanabandara76@gmail.com>
2020-10-27 09:26:46 +0530

    Add to ontap methods for buttons

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -4,16 +4,21 @@ class MyButton extends StatelessWidget {
   final color;
   final  textColor;
   final String buttonText;
-  MyButton(this.buttonText,this.color,this.textColor);
+  final buttonTapped;
+
+  MyButton(this.buttonText,this.color,this.textColor,this.buttonTapped);
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(8.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20.0),
-        child: Container(
-          color: color,
-          child: Center(child: Text(buttonText,style: TextStyle(color: textColor,fontWeight: FontWeight.bold,fontSize: 20.0),),),
+    return GestureDetector(
+      onTap: buttonTapped,
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Container(
+            color: color,
+            child: Center(child: Text(buttonText,style: TextStyle(color: textColor,fontWeight: FontWeight.bold,fontSize: 20.0),),),
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  var userQuestion ="";
+  var userAnswers ="";
+
+
   final List<String> buttons= [
     'C','DEL','%','/',
     '9','8','7','x',
@@ -36,7 +41,25 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           Expanded(
-              child:Container()
+              child:Container(
+                child: Column(
+
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(height: 10.0,),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(userQuestion,style: TextStyle(fontSize: 20.0),),
+                    ),
+                    Container(
+                      alignment: Alignment.centerRight,
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(userAnswers,style: TextStyle(fontSize: 20.0),),
+                    )
+                  ],
+                ),
+              )
           ),
           Expanded(
             flex: 4,
@@ -46,14 +69,30 @@ class _HomePageState extends State<HomePage> {
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
                   itemBuilder: (BuildContext context , int index){
                   if(index==0){
-                    return MyButton(buttons[index],Colors.green ,Colors.white);
+                    return MyButton( buttons[index],Colors.green ,Colors.white,(){
+                    setState((){
+                    userQuestion +=buttons[index];
+                    });
+                    },);
                   }
                   else if(index==1){
-                    return MyButton(buttons[index],Colors.red ,Colors.white );
+                    return MyButton(buttons[index],Colors.red ,Colors.white,
+                      (){
+                    setState((){
+                    userQuestion +=buttons[index];
+                    });
+                    }, );
                   }
                   else{
-                    return MyButton(buttons[index],isOprerator(buttons[index])? Colors.deepPurple:Colors.deepPurple[50] ,
-                        isOprerator(buttons[index])? Colors.white:Colors.deepPurple );
+                    return MyButton(
+
+
+                        buttons[index],isOprerator(buttons[index])? Colors.deepPurple:Colors.deepPurple[50] ,
+                        isOprerator(buttons[index])? Colors.white:Colors.deepPurple ,(){
+                    setState((){
+                    userQuestion +=buttons[index];
+                    });
+                    },);
                   }
 
 
